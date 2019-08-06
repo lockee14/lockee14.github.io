acc2daaa2a0eb818935b3353f4c54c98da9365b1
lockee14 <lockee14@msn.com>
2019-08-06 09:54:43 +0900

    first commit of my github page

---
+++ assets/js/slide.js
@@ -0,0 +1,7 @@
+window.onload = (e) => {
+    console.log("ONLOAD EVENT: ", e);
+    // var carousels = document.getElementsByClassName("CarouselContainer");
+    // carousels.forEach(element => {
+    //     element.a
+    // });
+}
